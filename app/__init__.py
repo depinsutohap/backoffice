72ac780ce0eb3caf8cae369eb780d4e3a01f03c8
Yehuda Dani Utomo <yeye@hop.cash>
2020-02-12 02:08:34 +0700

    Alligator 0023 -> Updating Validation on User's Auth and Update the SMTP class

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from sanic import Sanic
 from sanic_session import Session as _Session
-from sanic_mail import Sanic_Mail
 from sanic_jinja2 import SanicJinja2
 from sanic_auth import Auth, User
 from sanic_cors import CORS, cross_origin
@@ -14,11 +13,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.pool import NullPool
 
-
-# db = SQLAlchemy()
-# mongo = PyMongo()
-# csrf = CSRFProtect()
-
 app = Sanic(__name__)
 jinja = SanicJinja2()
 _auth = Auth(app)
@@ -26,7 +20,6 @@ _auth = Auth(app)
 engine = create_engine('mysql://hop_bo:!2345HopBo0005432!@157.230.46.218:3306/uta_hop_dev_db_000', poolclass=NullPool)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
-# sm=Sanic_Mail()
 
 # client = MongoClient("mongodb://hop_ag:!2345hopag0005432!@178.128.93.105:27017/uta_mongo_hop_db_000")
 # mongo = client.uta_mongo_hop_db_000
@@ -38,11 +31,9 @@ def create_app(config_name):
     app.static('/static', 'app/static')
 
     CORS(app, resources={r"/api-v1/*": {"origins": "*"}})
-    # CORS(app)
 
     config[config_name].init_app(app)
     _Session(app)
-    # sm.init_app(app)
     Session.configure(bind=engine)
     jinja.init_app(app, pkg_path='templates')
 
--- config.py
+++ config.py
@@ -2,13 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    # SECRET_KEY = "Learn from the data and HOP to grow"
-    # MAIL_SENDER = "hello@hop.cash"
-    # MAIL_SENDER_PASSWORD = "!2345hop5432!"
-    # MAIL_SEND_HOST = "mx-s3.vivawebhost.com"
-    # MAIL_SEND_PORT = 465
-    # MAIL_TLS = True
-
+    SECRET_KEY = 'learn from the data and hop to grow'
     @staticmethod
     def init_app(app):
         pass
