ffe3d5f304a977ef6b0992de0ee0b63f468a0f5a
Intan Ardipramanova <ardi@hop.cash>
2020-02-12 18:22:07 +0700

    Anaconda 006 -> updates directory

--- app/api_1/report.py
+++ app/api_1/report.py
@@ -737,7 +737,6 @@ async def _download(request):
         session = Session()
         try:
             if user is not None and user.verify_token(apidata['token'][0]):
-                # directory = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'file_secure\\excels\\')
                 # app.static(apidata['filename'], '/file_secure/excels/', stream_large_files=True)
                 return await response.file(excelDir+apidata['filename'][0])
                 # return send_file('/file_secure/excels/'+apidata['filename'][0], as_attachment=True)
